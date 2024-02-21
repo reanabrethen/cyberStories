6e7a81063130c8bb8c706218a79552b300178ebd
Admin <reanajb@gmail.com>
2024-02-21 12:31:49 -0500

    story details & stories loaded on DOM

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const storyIdURL = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+
 
 const body = document.querySelector('body');
  
@@ -10,6 +12,13 @@ cardBody.className = 'cardBody'
 card.appendChild(cardBody)
 body.appendChild(card)
 
+let titleOfStory
+let userName
+let numOfComments
+let scorePoints
+
+
+// cardBody.innerHTML = '230 points- "Story of Coding" , user101, 20 comments'
 
 // function createCard(element){
 //    return  card.appendChild(element) 
@@ -20,11 +29,63 @@ body.appendChild(card)
 // }
 // ;
 
-// function addNewStory(text){
-//     cardBody.innerText = `${text}`
-//     return cardBody
-// }
 
-// const newStory = addNewStory('How to learn coding - 200 points , 50 comments, submitted by user101')
 
-// addNewStory(newStory)
+const newStory = addNewStory('"Learn Coding" - 200 points \n 50 comments - submitted by user101')
+
+// createCard(newStory)
+console.log(newStory)
+
+
+fetch(storyIdURL)
+    .then((rawResponse)=>{
+        return rawResponse.json()
+    })
+    .then((arrayOfStoryIds) => {
+        console.log(arrayOfStoryIds)
+        arrayOfStoryIds.length = 100
+        
+        let arrayOfPromises = []
+        //loop through arr 
+        for(let id of arrayOfStoryIds){
+             //make new fetches for each one of them
+             arrayOfPromises.push(fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`))
+        }
+       console.log(arrayOfPromises)
+        //Promise.all --> arr would push fetches
+        return Promise.all(arrayOfPromises) //returns arr of responses
+        
+        //for loop to loop through arr and attach invidual ID 
+    })
+     //building website
+        //creating elements
+        //loop through
+        //template literals
+    
+    .then((results) => Promise.all(results.map(res => res.json())))
+    .then((arrayOfStoryObjs) => {
+         console.log(arrayOfStoryObjs)
+            for(let story of arrayOfStoryObjs){
+                addNewStory(story)
+            }
+        //  let storyDetailURL = []
+         
+            //  storyDetailURL.push(fetch(`https://hacker-news.firebaseio.com/v0/item/${arrayOfStoryObjs[url]}.json?print=pretty`))
+         
+        //  console.log(storyDetailURL)
+        //  return Promise.all(storyDetailURL)
+        // })
+        // .then((results) => Promise.all(results.map(res => res.json())))
+        })
+
+    
+        function addNewStory(storyObj){
+        const cardBody = document.createElement('div')
+        cardBody.className = 'cardBody'
+        //cardBody is called globally that creates new div/element
+    //setting text of card
+    cardBody.innerHTML = `<a href="${storyObj.url}">${storyObj.title}</a>`
+    //return element
+    body.appendChild(cardBody)
+}
+
